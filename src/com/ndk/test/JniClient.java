856ba7f1d2a602e1f1661ca6a22d0fbcf9e9573a
baideyi <baideyi@163.com>
2015-03-30 12:21:07 +0800

    testt

--- src/com/ndk/test/JniClient.java
+++ src/com/ndk/test/JniClient.java
@@ -2,5 +2,6 @@ package com.ndk.test;
 
 public class JniClient {
 	static public native String AddStr(String strA, String strB);
-    static public native int AddInt(int a, int b);
+    static public native int AddInt(int a, int b); 
+    
 }
